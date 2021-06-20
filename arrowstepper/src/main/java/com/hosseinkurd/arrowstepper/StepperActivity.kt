272da10ef24d0927a5f29365b4219e90a64cb037
Kurd <ho.kurd@gmail.com>
2021-06-20 19:20:57 +0430

    basics

---
+++ arrowstepper/src/main/java/com/hosseinkurd/arrowstepper/StepperActivity.kt
@@ -0,0 +1,11 @@
+package com.hosseinkurd.arrowstepper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StepperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stepper)
+    }
+}
