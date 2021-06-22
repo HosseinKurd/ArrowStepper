11a0a11e2e06d8efa5ec2af239b8674b3e4ad8b5
Kurd <ho.kurd@gmail.com>
2021-06-22 13:48:14 +0430

    Draw shape + clean up

--- arrowstepper/src/main/java/com/hosseinkurd/arrowstepper/component/ShitItem.kt
+++ arrowstepper/src/main/java/com/hosseinkurd/arrowstepper/component/ShitItem.kt
@@ -18,34 +18,18 @@ class ShitItem @JvmOverloads constructor(
     defStyleAttr
 ) {
 
+    private val paint = Paint()
+    private val path = Path()
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        /*val x: Int = (width * 0.09).toInt()
-        val y: Int = (height * 0.5).toInt()
-
-        val paint = Paint()
-        paint.color = Color.RED
-        paint.style = Paint.Style.FILL
-        paint.isAntiAlias = true
-
-        val p1 = Point(0, 0)
-        val p2 = Point(x, y)
-        val p3 = Point(0, height)
-
-        val path = Path()
-        path.fillType = Path.FillType.EVEN_ODD
-        path.moveTo(p1.x.toFloat(), p1.y.toFloat())
-        path.lineTo(p2.x.toFloat(), p2.y.toFloat())
-        path.lineTo(p3.x.toFloat(), p3.y.toFloat())
-        path.close()*/
-
-        val paint = Paint().apply {
+        paint.apply {
             color = Color.argb(100, 0, 220, 220)
             style = Paint.Style.FILL
             isAntiAlias = true
         }
 
-        val path = Path().apply {
+        path.apply {
             moveTo(0f, 0f)
             lineTo((width - (width * 0.09)).toFloat(), 0F)
             lineTo(width.toFloat(), (height * 0.5).toFloat())
---
+++ arrowstepper/src/main/java/com/hosseinkurd/arrowstepper/component/enums/ShitState.kt
@@ -0,0 +1,6 @@
+package com.hosseinkurd.arrowstepper.component.enums
+
+enum class ShitState {
+    SHIT_EXPANDED,
+    SHIT_COLLAPSED
+}
