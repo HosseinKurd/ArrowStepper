a3b33cc6fb251fa31bdc55fed3f2cfee6c238e69
Kurd <ho.kurd@gmail.com>
2021-06-22 12:38:04 +0430

    Draw shape

--- arrowstepper/src/main/java/com/hosseinkurd/arrowstepper/component/ShitItem.kt
+++ arrowstepper/src/main/java/com/hosseinkurd/arrowstepper/component/ShitItem.kt
@@ -1,7 +1,10 @@
 package com.hosseinkurd.arrowstepper.component
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
@@ -16,14 +19,9 @@ class ShitItem @JvmOverloads constructor(
 ) {
 
     override fun onDraw(canvas: Canvas?) {
-        val x: Int = (width * 0.9).toInt()
+        super.onDraw(canvas)
+        /*val x: Int = (width * 0.09).toInt()
         val y: Int = (height * 0.5).toInt()
-
-        /*val paint = Paint()
-        paint.color = Color.RED
-        paint.style = Paint.Style.FILL
-        paint.isAntiAlias = true
-        canvas!!.drawLine(0f, 0f, x, y, paint)*/
 
         val paint = Paint()
         paint.color = Color.RED
@@ -32,17 +30,31 @@ class ShitItem @JvmOverloads constructor(
 
         val p1 = Point(0, 0)
         val p2 = Point(x, y)
-        val p3 = Point(x + width, y)
+        val p3 = Point(0, height)
 
         val path = Path()
         path.fillType = Path.FillType.EVEN_ODD
         path.moveTo(p1.x.toFloat(), p1.y.toFloat())
         path.lineTo(p2.x.toFloat(), p2.y.toFloat())
         path.lineTo(p3.x.toFloat(), p3.y.toFloat())
-        path.close()
+        path.close()*/
+
+        val paint = Paint().apply {
+            color = Color.argb(100, 0, 220, 220)
+            style = Paint.Style.FILL
+            isAntiAlias = true
+        }
+
+        val path = Path().apply {
+            moveTo(0f, 0f)
+            lineTo((width - (width * 0.09)).toFloat(), 0F)
+            lineTo(width.toFloat(), (height * 0.5).toFloat())
+            lineTo((width - (width * 0.09)).toFloat(), height.toFloat())
+            lineTo(0f, height.toFloat())
+            lineTo((width * 0.09).toFloat(), (height * 0.5).toFloat())
+        }
 
         canvas!!.drawPath(path, paint)
 
-        super.onDraw(canvas)
     }
 }
--- arrowstepper/src/main/java/com/hosseinkurd/arrowstepper/component/ShitStepper.kt
+++ arrowstepper/src/main/java/com/hosseinkurd/arrowstepper/component/ShitStepper.kt
@@ -1,14 +1,11 @@
 package com.hosseinkurd.arrowstepper.component
 
 import android.content.Context
-import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.widget.LinearLayoutCompat
 import com.hosseinkurd.arrowstepper.R
-import kotlin.random.Random
 
 class ShitStepper @JvmOverloads constructor(
     context: Context,
@@ -22,33 +19,23 @@ class ShitStepper @JvmOverloads constructor(
     ) {
 
     private val mainView: View = inflate(context, R.layout.stepper_view, this)
+    private val childWidth: Int = 100
+
 
     fun addShit() {
         val linearLayoutShitHolder: LinearLayout =
             mainView.findViewById(R.id.linearLayoutShitHolder)
-        var a = 0
-        while (a < linearLayoutShitHolder.childCount) {
-            val currentChildId = linearLayoutShitHolder.getChildAt(a).id
-            println("ShitStepper >> current child id : $currentChildId")
-            a++
-        }
-        val lastChildId =
-            linearLayoutShitHolder.getChildAt(linearLayoutShitHolder.childCount - 1).id
-        println("ShitStepper >> last child id : $lastChildId")
-        linearLayoutShitHolder.addView(getNewChild())
+        linearLayoutShitHolder.addView(getNewChild(linearLayoutShitHolder.childCount == 0))
     }
 
-    private fun getNewChild(): View {
+    private fun getNewChild(isFirstChild: Boolean): View {
+        val childWidthInDP = getInDP(childWidth)
         val view = ShitItem(context)
-        val layoutParams = LinearLayout.LayoutParams(getInDP(100), getInDP(50))
+        val layoutParams = LinearLayout.LayoutParams(childWidthInDP, LayoutParams.MATCH_PARENT)
+        if (isFirstChild.not()) {
+            layoutParams.marginStart = (childWidthInDP * -0.05).toInt()
+        }
         view.layoutParams = layoutParams
-        /*view.setBackgroundColor(
-            Color.rgb(
-                getRandomNumberInRange(),
-                getRandomNumberInRange(),
-                getRandomNumberInRange()
-            )
-        )*/
         view.id = View.generateViewId()
         println("ShitStepper >> new child id : ${view.id}")
         return view
@@ -56,19 +43,5 @@ class ShitStepper @JvmOverloads constructor(
 
     private fun getInDP(number: Int): Int {
         return (number * context.resources.displayMetrics.density + 0.5f).toInt()
-    }
-
-    private fun getRandomNumberInRange(): Int {
-        val min = 0
-        val max = 255
-        return Random.nextInt(max - min + 1) + min
-    }
-
-    fun findAndSetFirstValidId(): Int {
-        var randomId: Int
-        do {
-            randomId = Random.nextInt()
-        } while (findViewById<View>(randomId) != null)
-        return randomId
     }
 }
